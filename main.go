26959b8a8f374d285e14ea992a5c52de800215a2
simp7 <JHParkOfficial@protonmail.com>
2025-01-04 12:59:01 +0900

    Step 2

--- main.go
+++ main.go
@@ -2,29 +2,54 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
+	"io"
 	"os"
 )
 
 func main() {
-	args := os.Args
-	if len(args) < 2 {
-		fmt.Println("not implemented yet")
-		os.Exit(1)
-	}
+	var reader io.Reader = os.Stdin
+	var writer io.Writer = os.Stdout
+	var err error
 
-	file, err := os.Open(args[1])
-	if err != nil {
-		fmt.Println("Error: ", err)
-		os.Exit(1)
+	args := os.Args
+
+	switch len(args) {
+	case 1:
+	case 2:
+		if args[1] == "-" {
+			break
+		}
+		reader, err = os.Open(args[1])
+		if err != nil {
+			fmt.Println("Error: ", err)
+			os.Exit(1)
+		}
+	default:
+		readers := make([]io.Reader, 0)
+		for i := 1; i < len(args)-2; i++ {
+			readers[i-1], err = os.Open(args[i])
+			if err != nil {
+				fmt.Println("Error: ", err)
+				os.Exit(1)
+			}
+		}
+		reader = io.MultiReader(readers...)
+		writer, err = os.Open(args[len(args)-1])
+		if errors.Is(err, os.ErrNotExist) {
+			err = nil
+			writer, err = os.Create(args[len(args)-1])
+		}
+
+		if err != nil {
+			fmt.Println("Error: ", err)
+			os.Exit(1)
+		}
 	}
-	defer file.Close()
 
 	u := NewUnique()
-	result := u.Execute(file)
-	for _, line := range result {
-		fmt.Print(line)
-	}
+	u.Execute(reader, writer)
 	os.Exit(0)
 }
 
@@ -37,16 +62,15 @@ func NewUnique() *unique {
 	return u
 }
 
-func (u *unique) Execute(file *os.File) []string {
-	result := make([]string, 0)
-	reader := bufio.NewReader(file)
+func (u *unique) Execute(input io.Reader, output io.Writer) {
+	reader := bufio.NewReader(input)
 	for {
 		line, err := reader.ReadString('\n')
 		if err != nil {
-			return result
+			return
 		}
 		if u.prev != line {
-			result = append(result, line)
+			output.Write([]byte(line))
 			u.prev = line
 		}
 	}
